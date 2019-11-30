5efe4ac365e9bd5c2d8d69ba59682190a62b4498
juniad <azizijunaid@gmail.com>
2019-11-30 17:38:17 +0500

    solved

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,5 +1,23 @@
 function areSimilar(a: number[], b: number[]): boolean {
-   
+   let c : number[] = [];
+   let d : number[] = [];
+
+    if(a.toString() === b.toString()){
+       return true
+   }
+
+   for (let index = 0; index < a.length; index++) { // 0 value 1 !== 2
+       if(a[index] !== b[index]){
+            c.push(a[index])
+            d.push(b[index])
+       }
+   }
+
+   d.reverse();
+   if(c.length === 2 && (c.toString() === d.toString()){
+       return true
+   } 
+   return false;
 }
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
